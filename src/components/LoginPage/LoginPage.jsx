5100fbf28aa72273dcf53381defdb8411ffef36f
DevelopRf <kzrf91@gmail.com>
2024-05-03 18:20:39 +0400

    instagram clone

---
+++ src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,17 @@
+export default function LoginPage() {
+    return (
+        <section className="mt-8">
+            <div className="w-full h-full flex justify-center p-2">
+                <div style={{ backgroundImage: "url('https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones-2x.png?__makehaste_cache_breaker=73SVAexZgBW')" }} className="w-[380px] h-[581px] bg-[length:468px_634px] bg-[top_left_-46px] relative">
+                    <div className="absolute w-full h-full z-10">
+                        <img src="https://www.instagram.com/images/instagram/xig/homepage/screenshots/screenshot1.png?__d=www" alt="" className="h-full"/>
+                       
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <div></div>
+                </div>
+            </div>
+        </section>
+    )
+}
